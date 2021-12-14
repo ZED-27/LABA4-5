81573ae49310f6e7cf2a0845df02dfcde8421cf6
ZED-27 <staiband2116@gmail.com>
2021-12-14 05:33:07 +0300

    first Commit

---
+++ src/MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame{
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
